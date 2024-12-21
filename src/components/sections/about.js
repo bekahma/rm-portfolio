05eb0b1b682476f3b2947bdae81f25da9a2ee152
Rebecca Ma <r69ma@uwaterloo.ca>
2024-12-20 19:30:54 -0500

    edit about section

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -125,7 +125,7 @@ const About = () => {
     sr.reveal(revealContainer.current, srConfig());
   }, []);
 
-  const skills = ['JavaScript (ES6+)', 'TypeScript', 'React', 'Eleventy', 'Node.js', 'WordPress'];
+  // const skills = ['JavaScript (ES6+)', 'TypeScript', 'React', 'Eleventy', 'Node.js', 'WordPress'];
 
   return (
     <StyledAboutSection id="about" ref={revealContainer}>
@@ -135,45 +135,34 @@ const About = () => {
         <StyledText>
           <div>
             <p>
-              Hello! My name is Brittany and I enjoy creating things that live on the internet. My
-              interest in web development started back in 2012 when I decided to try editing custom
-              Tumblr themes — turns out hacking together a custom reblog button taught me a lot
-              about HTML &amp; CSS!
+              Hi, I’m Rebecca! I recently graduated with a degree in{' '}
+              <a href="https://uwaterloo.ca/biomedical-engineering/">
+                Biomedical Engineering from the University of Waterloo
+              </a>
+              , where I explored topics like the gait cycle, fluid dynamics in blood vessels, and
+              three-point bending tests for bones.
             </p>
 
             <p>
-              Fast-forward to today, and I’ve had the privilege of working at{' '}
-              <a href="https://us.mullenlowe.com/">an advertising agency</a>,{' '}
-              <a href="https://starry.com/">a start-up</a>,{' '}
-              <a href="https://www.apple.com/">a huge corporation</a>, and{' '}
-              <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
-              main focus these days is building accessible, inclusive products and digital
-              experiences at <a href="https://upstatement.com/">Upstatement</a> for a variety of
-              clients.
+              During my studies, I had the opportunity to explore different roles in software
+              development and product design. While I learned a lot from each experience, I’m now
+              shifting my focus to something more technical as I continue my studies in Computer
+              Engineering with a specialization in machine learning.
             </p>
 
             <p>
-              I also recently{' '}
-              <a href="https://www.newline.co/courses/build-a-spotify-connected-app">
-                launched a course
-              </a>{' '}
-              that covers everything you need to build a web app with the Spotify API using Node
-              &amp; React.
+              When I'm not in front of a computer screen, you can find me exploring art galleries,
+              scrolling IG reels to find new recipes to cook, and snapping photos on my disposable
+              camera.
             </p>
-
-            <p>Here are a few technologies I’ve been working with recently:</p>
           </div>
-
-          <ul className="skills-list">
-            {skills && skills.map((skill, i) => <li key={i}>{skill}</li>)}
-          </ul>
         </StyledText>
 
         <StyledPic>
           <div className="wrapper">
             <StaticImage
               className="img"
-              src="../../images/me.jpg"
+              src="../../images/headshot.jpg"
               width={500}
               quality={95}
               formats={['AUTO', 'WEBP', 'AVIF']}
@@ -187,3 +176,12 @@ const About = () => {
 };
 
 export default About;
+
+/*
+commenting out skills section for later
+<p>Here are a few technologies I’ve been working with recently:</p>
+
+<ul className="skills-list">
+  {skills && skills.map((skill, i) => <li key={i}>{skill}</li>)}
+</ul>
+*/
--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -60,28 +60,24 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
-  const three = <h3 className="big-heading">I build things for the web.</h3>;
+  const two = <h2 className="big-heading">Rebecca Ma.</h2>;
+  const three = <h3>Master's student in Electrical & Computer Engineering.</h3>;
   const four = (
     <>
       <p>
-        I’m a software engineer specializing in building (and occasionally designing) exceptional
-        digital experiences. Currently, I’m focused on building accessible, human-centered products
-        at{' '}
-        <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
-          Upstatement
-        </a>
-        .
+        I am an engineer and researcher dedicated to tackling challenging problems and harnessing
+        evolving technology to enhance the quality of life for others.
       </p>
     </>
   );
   const five = (
     <a
       className="email-link"
-      href="https://www.newline.co/courses/build-a-spotify-connected-app"
+      href="https://www.linkedin.com/in/r-ma/"
       target="_blank"
-      rel="noreferrer">
-      Check out my course!
+      rel="noreferrer"
+    >
+      Let's connect!
     </a>
   );
 
